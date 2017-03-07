7f28ec2f8c9e424974aff07347f77108cfeb481b
Antoshka <jnt2007@ya.ru>
2017-03-07 09:57:45 +0200

    fixed path in wsgi

--- autologger/wsgi.py
+++ autologger/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.append('/home/anton/www/autologger')
+sys.path.append('/home/anton/www/autologger_ipmp')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autologger.settings")
 
 from django.core.wsgi import get_wsgi_application
